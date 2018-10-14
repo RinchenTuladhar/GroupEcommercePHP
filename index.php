c6d21abe97de75d6ff73a563a1409083965c6422
= <rinchentul@gmail.com>
2018-10-14 23:34:58 +0100

    Home page background

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ if (session_status () == PHP_SESSION_NONE) {
 
 <html>
 <head>
+    <?php include 'api/scripts.php'; ?>
 <title>E-Commerce CMS</title>
-    <?php include 'includes.php'; ?>
 </head>
 
 <body>
 <?php include 'navbar.php'; ?>
-<div class="container">
-
+<div class="main">
+    <div class="main-image">
+        <img src="img/background-clothing-store.jpg">
+    </div>
+    <h1> Build a site with BuildMyStore.com</h1>
 </div>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -3,10 +3,32 @@
 
 <body>
 <div class="navigation-menu">
-  <header role="banner">
-      <div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Wedding Guru</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavBar" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
 
-      </div>
-  </header>
+            <div class="collapse navbar-collapse" id="mainNavBar">
+                <ul class="navbar-nav mr-auto navbar-left">
+                    <li class="nav-item">
+                        <a class="nav-link <?php if ($activePage=="index") {echo "active"; } else  {echo "noactive";}?>" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php if ($activePage=="features") {echo "active"; } else  {echo "noactive";}?>" href="venues.php"> Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php if ($activePage=="about") {echo "active"; } else  {echo "noactive";}?>" href="about.php"> About</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item dropdown">
+                            <a class="nav-link" href="login.php"><i class="fa fa-sign-in"></i> Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div> <!--.container-->
+    </nav>
 </div> <!--.navigation-menu-->
 </body>
