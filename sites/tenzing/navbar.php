577768572674bf42ac8eb933379e2e7a5e5c8cdd
= <rinchentul@gmail.com>
2018-12-24 13:12:05 +0000

    Basket and item view for theme1

---
+++ sites/tenzing/navbar.php
@@ -0,0 +1,73 @@
+<?php
+include 'api/db-access.php';
+
+$url = $_SERVER['REQUEST_URI'];
+$splitUrl = array_filter(explode('/', $url));
+
+$siteName;
+
+if($splitUrl[3] === "sites"){
+    $siteName = $splitUrl[4];
+}  else {
+    $siteName = $splitUrl[3];
+}
+
+$_SESSION["WebsiteDetails"] = $db->getWebsiteID($siteName);
+
+?>
+
+<body>
+<div class="navigation-menu">
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <a class="navbar-brand" href="#"><?php echo $siteName; ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavBar" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="mainNavBar">
+            <ul class="navbar-nav mr-auto navbar-left">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <?php
+                $listOfNavigation = $db->getCategoryNavigation($siteName);
+                while($row = $listOfNavigation->fetch_assoc()){
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.php?<?php echo $row['Title'];?>"><?php echo $row["Title"]?></a>
+                    </li>
+                    <?php
+                }
+                ?>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <?php if(isset($_SESSION["loggedin"])){
+                    ?>
+                    <?php if ($_SESSION['loggedin'] == null) {
+                        ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="signup.php">Sign Up</a>
+                        </li>
+                        <?php
+                    } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="basket.php"><i class="fa fa-shopping-cart"></i> £0.00</a>
+                        </li>
+                    <?php } ?>
+                    <?php
+                }else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">Sign Up</a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+    </nav>
+</div> <!--.navigation-menu-->
+</body>
