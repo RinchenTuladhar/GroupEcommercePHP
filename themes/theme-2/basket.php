45b15599ebce26faca1598a98f2980ec0fc27375
= <rinchentul@gmail.com>
2018-12-28 14:10:51 +0000

    Theme-2 basket UI

--- themes/theme-2/basket.php
+++ themes/theme-2/basket.php
@@ -16,6 +16,77 @@ $query = $_SERVER ['QUERY_STRING'];
 <div class="home-page">
     <?php include 'navbar.php'; ?>
     <div class="main">
+        <div class="basket-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Basket</h1>
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <img src="img/items/5c24dcb5accef.jpg">
+                                </td>
+                                <td>£45.00</td>
+                                <td>
+                                    <select class="form-control">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/items/5c24dcb5accef.jpg">
+                                </td>
+                                <td>£45.00</td>
+                                <td>
+                                    <select class="form-control">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/items/5c24dcb5accef.jpg">
+                                </td>
+                                <td>£45.00</td>
+                                <td>
+                                    <select class="form-control">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Checkout</h2>
+                        <hr/>
+                        <p>Total: £00.00</p>
+                        <input type="button" class="btn btn-default" value="Checkout">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
--- themes/theme-2/item.php
+++ themes/theme-2/item.php
@@ -14,8 +14,31 @@ $query = $_SERVER ['QUERY_STRING'];
 
 <body>
 <div class="home-page">
-    <?php include 'navbar.php'; ?>
+    <?php include 'navbar.php';
+
+    $itemArray = $db->getItemInformation($_SESSION["WebsiteDetails"]["WebsiteID"], $query);
+    $item = $itemArray->fetch_assoc();
+    ?>
     <div class="main">
+        <div class="item-container container">
+            <h1 class="item-title"><?php
+                echo $item["Name"];
+                ?></h1>
+            <div class="row">
+                <div class="col-md-3">
+                    <h2>Description</h2>
+                    <p><?php echo $item["Description"]; ?></p>
+                </div>
+                <div class="item-image col-md-6">
+                    <img class="item-image" src="img/items/<?php echo $item["ProductID"]; ?>.jpg">
+                </div>
+                <div class="col-md-3">
+                    <h2>Details</h2>
+                    <p>Price: £<?php echo $item["Price"]; ?></p>
+                    <input type="button" class="btn btn-default" value="Add To Basket">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
