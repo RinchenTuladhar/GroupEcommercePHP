7efd8ad875c94418e0c62d3953b99ba422ec70a2
Rinchen T <rinchentul@gmail.com>
2019-03-27 14:54:33 +0000

    AWS config added

--- api/db/config.php
+++ api/db/config.php
@@ -2,19 +2,21 @@
 /*
 define("DB_HOST", "mysql.cms.livjm.ac.uk");
 define("DB_USER", "cmprtula");
-define("DB_PASSWORD", "tr8prais");
+define("DB_PASSWORD", "lif6lati");
 define("DB_DATABASE", "cmprtula");
 
 
-define("DB_HOST", "commerce.c9l9bem1arqa.eu-west-2.rds.amazonaws.com");
-define("DB_USER", "rinchen");
-define("DB_PASSWORD", "12345qwert");
-define("DB_DATABASE", "commerce");
 
- */
+
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
 define("DB_DATABASE", "commerce");
+ */
 
+
+define("DB_HOST", "ecommerce.c9l9bem1arqa.eu-west-2.rds.amazonaws.com");
+define("DB_USER", "rinchen");
+define("DB_PASSWORD", "ecommerce");
+define("DB_DATABASE", "commerce");
 ?>
